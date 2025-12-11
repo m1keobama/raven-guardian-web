2c3d224ec3d897f9486a02b5a26098d3885a6519
m1keobama <blendernoob1337@gmail.com>
2025-12-11 13:29:16 +0100

    Define more environment variables in Vite config
    Added additional environment variables for frontend access.

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // Lädt Umgebungsvariablen aus der .env Datei
   const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // Damit process.env.API_KEY im Frontend funktioniert
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      // Damit diese Variablen im Frontend verfügbar sind, müssen wir sie hier definieren
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+      'process.env.ADMIN_USER_HASH': JSON.stringify(env.ADMIN_USER_HASH),
+      'process.env.ADMIN_PASS_HASH': JSON.stringify(env.ADMIN_PASS_HASH),
     }
   }
 })
